a9fb779b5eb17b7dd8b447ffa53e9e7c9a14d35e
e-psi-lon <theg41g@gmail.com>
2024-07-18 23:48:41 +0200

    refactor: `_unload` function python generated instead of mcfunction

--- user/link.py
+++ user/link.py
@@ -11,10 +11,28 @@ from .utils.abitities.functions import create_functions
 # Main function is run just before making finalyzing the build process (zip, headers, lang, ...)
 def main(config: dict) -> None:
 	database: dict[str, dict] = config["database"]
+	build_datapack = config["build_datapack"]
 	namespace: str = config["namespace"]
+	version = config["version"]
+	datapack_name: str = config["datapack_name"]
 	create_custom_craft(config)
 	generate_advancements(config)
 	register_settings(config)
 	create_functions(config)
+	write_to_file(f"{build_datapack}/data/{namespace}/function/_unload.mcfunction", f"""
+#scores
+scoreboard objectives remove {namespace}.settings
+#team
+team remove piglin
+#schedule
+schedule clear {namespace}:v{version}/abilities/piglin_schedule
+#tags
+tag @e[type=piglin,tag={namespace}.pig] remove {namespace}.pig
+tag @a[tag={namespace}.pig] remove {namespace}.pig
+
+tellraw @s [{'{"text": "['+datapack_name+']:","color": "green"}'},{'{"text": "The datapack has succesfully been uninstall","color": "white"}'}]
+datapack disable "file/{datapack_name.replace(" ", "")}_datapack.zip"
+datapack disable "file/{datapack_name.replace(" ", "")}_datapack"
+""")
 	
 
