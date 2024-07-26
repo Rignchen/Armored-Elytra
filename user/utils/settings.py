6cf3a86f3034d99ac7acec01532e446359cdb1fe
e-psi-lon <theg41g@gmail.com>
2024-07-26 13:02:50 +0200

    fix(settings): Reversed color and name in tellraw

--- user/utils/settings.py
+++ user/utils/settings.py
@@ -24,8 +24,8 @@ tellraw @s [{'{"text":"['+name+'] Settings:\\n","color":"green"}'}]
 	for score, desc_default in settings.items():
 		load += f"execute unless score #{score} {namespace}.settings = #{score} {namespace}.settings run scoreboard players set #{score} {namespace}.settings {desc_default[2]}\n"
 		settings_function += f"""
-execute if score #{score} {namespace}.settings matches 1 run tellraw @s [{'{"color":"white","text":"'+desc_default[0]+'"}'},{'{"clickEvent":{"action":"run_command","value":"/function '+namespace+':v'+version+'/update_config {\\\"value\\\":0, \\\"param\\\":\\\"'+score+'\\\"}"}'},"color":"red","hoverEvent":{'{"action":"show_text","value":[{"text":"A'+desc_default[1]+'"}]},"text":"[ ]"}'}]
-execute if score #{score} {namespace}.settings matches 0 run tellraw @s [{'{"color":"white","text":"'+desc_default[0]+'"}'},{'{"clickEvent":{"action":"run_command","value":"/function '+namespace+':v'+version+'/update_config {\\\"value\\\":1, \\\"param\\\":\\\"'+score+'\\\"}"}'},"color":"green","hoverEvent":{'{"action":"show_text","value":[{"text":"Dea'+desc_default[1]+'"}]},"text":"[x]"}'}]
+execute if score #{score} {namespace}.settings matches 1 run tellraw @s [{'{"color":"white","text":"'+desc_default[0]+'"}'},{'{"clickEvent":{"action":"run_command","value":"/function '+namespace+':v'+version+'/update_config {\\\"value\\\":0, \\\"param\\\":\\\"'+score+'\\\"}"}'},"color":"green","hoverEvent":{'{"action":"show_text","value":[{"text":"Dea'+desc_default[1]+'"}]},"text":"[x]"}'}]
+execute if score #{score} {namespace}.settings matches 0 run tellraw @s [{'{"color":"white","text":"'+desc_default[0]+'"}'},{'{"clickEvent":{"action":"run_command","value":"/function '+namespace+':v'+version+'/update_config {\\\"value\\\":1, \\\"param\\\":\\\"'+score+'\\\"}"}'},"color":"red","hoverEvent":{'{"action":"show_text","value":[{"text":"A'+desc_default[1]+'"}]},"text":"[ ]"}'}]
 
 """
 	write_to_versioned_file(config, "load/secondary", load, prepend=True)
